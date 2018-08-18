ca2ef6c9db1412e13567f4fd1a6eba099db81fdf
iw.jhun <dlfdnd0725@gmail.com>
2018-08-18 10:42:24 +0900

    init

---
+++ src/main/java/com/glqdlt/bookstorage/BookStorageApplication.java
@@ -0,0 +1,12 @@
+package com.glqdlt.bookstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStorageApplication.class, args);
+    }
+}
