8dfac518d8900a576208ccf17accecf3dfd7f58e
iw.jhun <dlfdnd0725@gmail.com>
2018-08-18 15:05:04 +0900

    User엔티티

--- src/main/java/com/glqdlt/bookstorage/presistence/User.java
+++ src/main/java/com/glqdlt/bookstorage/presistence/User.java
@@ -2,14 +2,23 @@ package com.glqdlt.bookstorage.presistence;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Data
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long no;
 
     private String id;
     private String password;
     private String name;
+    private String email;
 
 
 }
